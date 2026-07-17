b4e866b282d22f7a016111ef865bb2f3735034c3
HarshadPanhale7 <harshadpanhale7@gmail.com>
2026-07-17 17:32:06 +0530

    Create init_database.sql

---
+++ Scripts/init_database.sql
@@ -0,0 +1,28 @@
+--===================================
+-- CREATE DATABASE & SCHEMAS
+--===================================
+  /*
+SCRIPT PURPOSE: 
+      This script creates a new database named 'DATAWAREHOSUSE' . Aditionally , the script sets up three schemas within the 
+      the database  which is : 'bronze' , 'silver' and  'gold'.
+*/
+
+USE MASTER;
+
+--=====================
+--CREATING OUR DATABASE 
+--=====================
+CREATE DATABASE DATAWAREHOUSE
+
+USE DATAWAREHOUSE
+
+--==================================
+--CREATING SCHEMAS FOR OUR DATABASE
+--==================================
+
+CREATE SCHEMA bronze ;
+GO							        --Go is just a separater that tells SQL to run the 1st whole code and then jump on next code
+CREATE SCHEMA silver;		--Go separates batches when working with multiple SQL statements 
+GO
+CREATE SCHEMA gold ;
+GO
