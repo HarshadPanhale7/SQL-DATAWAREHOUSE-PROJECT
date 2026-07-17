202e9561f0da2e5574803335adcf10ce261389b9
HarshadPanhale7 <harshadpanhale7@gmail.com>
2026-07-17 17:33:43 +0530

    Update init_database.sql

--- Scripts/init_database.sql
+++ Scripts/init_database.sql
@@ -21,8 +21,10 @@ USE DATAWAREHOUSE
 --==================================
 
 CREATE SCHEMA bronze ;
-GO							        --Go is just a separater that tells SQL to run the 1st whole code and then jump on next code
-CREATE SCHEMA silver;		--Go separates batches when working with multiple SQL statements 
+GO						           --Go is just a separater that tells SQL to run the 1st whole code and then jump on next code
+                        --Go separates batches when working with multiple SQL statements 
+CREATE SCHEMA silver;		
 GO
+  
 CREATE SCHEMA gold ;
 GO
